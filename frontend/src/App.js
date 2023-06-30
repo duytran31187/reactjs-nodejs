03ad788c725b4b567ba8125f3e7738bac1c99286
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-30 16:57:45 +0700

    customers class base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/Home'
+import RootLayout from './pages/Root'
+import ProductsPage from './pages/Products'
+import ErrorPage from './pages/Error'
+import ProductDetailPage from './pages/ProductDetail'
+import EventsPage, { loader as eventsLoader } from './pages/Events/Events'
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction
+} from './pages/Events/EventDetail'
+import FoodPage from './pages/Food'
+import AuthenticationPage, { action as authAction } from './pages/Authentication'
+import EventsRootLayout from './pages/Events/EventRoot'
+import EditEventPage from './pages/Events/EditEvent'
+import { action as manipulateEventAction } from './components/Events/EventForm'
+import NewEventPage from './pages/Events/NewEvent'
+import { action as logoutAction } from './pages/Logout'
+import { checkAuthLoader, tokenLoader } from './util/auth'
+import Users from './components/Customers/Users'
+import UsersPage from './pages/Users'
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    id: 'rootRouter',
+    loader: tokenLoader,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> },
+      { path: '/products/:productId', element: <ProductDetailPage /> },
+      { path: '/foods', element: <FoodPage /> },
+      {
+        path: 'auth',
+        element: <AuthenticationPage />,
+        action: authAction
+      },
+      {
+        path: 'logout',
+        action: logoutAction
+      },
+      {
+        path: 'events',
+        element: <EventsRootLayout />,
+        children: [
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader
+          },
+          {
+            path: ':eventId',
+            id: 'event-detail',
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction
+              },
+              {
+                path: 'edit',
+                element: <EditEventPage />,
+                action: manipulateEventAction
+              }
+            ]
+          },
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+            loader: checkAuthLoader
+          }
+        ]
+      },
+      { path: '/users', element: <UsersPage /> }
+    ]
+  }
+])
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <RouterProvider router={router} />
+  )
 }
 
-export default App;
+export default App
