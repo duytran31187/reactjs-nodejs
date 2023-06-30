c1cc6f4652ad0d39e811467d82cb90e69970b943
Duy Tran <duy.tranquoc@gforces.auto>
2023-07-01 05:50:47 +0700

    Foodpage class

--- frontend/src/pages/Food.js
+++ frontend/src/pages/Food.js
@@ -4,27 +4,53 @@ import Cart from "../components/Cart/Cart";
 import Header from "../components/Layout/Header";
 import Meals from "../components/Meals/Meals";
 
-const FoodPage = props => {
-    const [cartIsShown, setCartIsShown] = React.useState(false);
-
-    const showCartHandler = () => {
-        console.log("showCart");
-        setCartIsShown(true);
-    };
-
-    const hideCartHandler = () => {
-        console.log("hideCartHandler");
-        setCartIsShown(false);
-    };
-    return (
-        <CartProvider>
-            {cartIsShown && <Cart onClose={hideCartHandler}/>}
-            <Header onShowCart={showCartHandler}/>
-            
-            <main>
-                <Meals />
-            </main>
-      </CartProvider>
-    )
+class FoodPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cartIsShown: false,
+        }
+    }
+    showCartHandler = () => {
+        this.setState({cartIsShown: true})
+    }
+    hideCartHandler = () => {
+        this.setState({cartIsShown: false})
+    }
+    render() {
+        return (
+            <CartProvider>
+                {this.state.cartIsShown && <Cart onClose={this.hideCartHandler}/>}
+                <Header onShowCart={this.showCartHandler}/>
+                
+                <main>
+                    <Meals />
+                </main>
+          </CartProvider>
+        )
+    }
 }
+// const FoodPage = props => {
+//     const [cartIsShown, setCartIsShown] = React.useState(false);
+
+//     const showCartHandler = () => {
+//         console.log("showCart");
+//         setCartIsShown(true);
+//     };
+
+//     const hideCartHandler = () => {
+//         console.log("hideCartHandler");
+//         setCartIsShown(false);
+//     };
+//     return (
+//         <CartProvider>
+//             {cartIsShown && <Cart onClose={hideCartHandler}/>}
+//             <Header onShowCart={showCartHandler}/>
+            
+//             <main>
+//                 <Meals />
+//             </main>
+//       </CartProvider>
+//     )
+// }
 export default FoodPage;
