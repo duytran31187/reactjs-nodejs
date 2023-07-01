faf4dd522f8193acb4d56244236f8997eb8c13b4
Duy Tran <duy.tranquoc@gforces.auto>
2023-07-01 16:06:26 +0700

    [redux] store as wrapper

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client';
-
+import { Provider } from 'react-redux';
+import appStore from "./redux/store"; // as export default in the index file so we can name any varialbe: app, appStore....
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Provider store={appStore}><App /></Provider>);
